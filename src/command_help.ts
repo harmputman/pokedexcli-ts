a70b897d4fedc4550743965ebd50702cbea84ae6
Harm Putman <h.putman@am-impact.nl>
2026-03-19 09:36:16 +0100

    CH2-L1

---
+++ src/command_help.ts
@@ -0,0 +1,11 @@
+import { State } from "./state.js";
+
+export function commandHelp(state: State): void {
+    console.log("Welcome to the Pokedex!");
+    console.log("Usage:");
+    console.log();
+    for (const cmd of Object.values(state.commands)) {
+        console.log(`${cmd.name}: ${cmd.description}`)
+    }
+    console.log();
+}
---
+++ src/repl.ts
@@ -0,0 +1,42 @@
+import { State } from "./state.js";
+
+export function cleanInput(input: string): string[] {
+  return input
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+}
+
+export function startREPL(state: State): void {
+    state.readline.prompt();
+
+    state.readline.on("line", async (input: string) => {
+        const words = cleanInput(input);
+
+        if (words.length === 0) {
+            state.readline.prompt();
+            return;
+        }
+
+        const commandName = words[0];
+
+        const cmd = state.commands[commandName];
+
+        if (!cmd) {
+            console.log(
+                `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+            );
+            state.readline.prompt();
+            return;
+        }
+
+        try {
+            cmd.callback(state);
+        } catch (e) {
+            console.log(e);
+        }
+
+        state.readline.prompt();
+    });
+}
